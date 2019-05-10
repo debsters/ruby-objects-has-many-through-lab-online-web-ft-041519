8d18ed2873573f374d118f42189c23b1c6736631
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-05-10 23:10:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,18 +13,3 @@ class Song
     @@all
   end
 end
-class Song
-  attr_accessor :name, :artist, :genre
-
-   @@all = []
-  def initialize(name, artist, genre)
-    @name = name
-    @artist = artist
-    @genre = genre
-    @@all << self
-  end
-
-   def self.all
-    @@all
-  end
-end
